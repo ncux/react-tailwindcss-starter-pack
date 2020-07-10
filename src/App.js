53b1eac9209f1420036f5f8043d5eaa9cb5303c2
ncux <malabatalent@gmail.com>
2020-07-10 17:36:05 +0800

    react tailwindcss starter pack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+          <div className="container mx-auto my-4 py-4">
+
+          </div>
+  );
+}
+
+export default App;
---
+++ src/search/search.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+export const Search = ({ searchPictures }) => {
+
+    const [query, setQuery] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(query === '') return;
+        searchPictures(query);
+        setQuery('');
+    };
+
+    return (
+        <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
+            <form onSubmit={ onSubmit } className="w-full max-w-sm">
+                <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+                    <input type="text"
+                           onChange={ event => setQuery(event.target.value) }
+                           className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                           placeholder="Search for pictures"
+                    />
+                    <button type="submit" className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+
+};
+
